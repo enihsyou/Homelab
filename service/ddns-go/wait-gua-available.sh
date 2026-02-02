0da5464bb132f046b9b649a467fb7a78895b07d2
九条涼果 <enihsyou@gmail.com>
2026-02-02 17:25:46 +0800

    fix the DDNS failed wieh non GUA available problem when container starts

---
+++ service/ddns-go/wait-gua-available.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# this file is meant to be used as the entrypoint of a ddns-go container
+# refer to service/alist/docker-compose.yml for an example
+
+# exec ddns-go only when global IPv6 is available
+while true; do
+    if ip -6 addr show scope global | grep 'inet6 [23]'; then
+        sleep 3
+        ping -c1 -W5 $(ip -6 route | awk '/^[23]/{sub(/\/.*/, "", $1); print $1 "1"; exit}')
+        exec /app/ddns-go -f 600
+    fi
+done
